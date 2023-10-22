0cfa825c5dad9825af5faae8935b8b05817f6b59
startup-dreamer <s_kbharbhunja@me.iitr.ac.in>
2023-10-22 18:18:37 +0530

    add Readme and configs

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      vt: ["VT323"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
